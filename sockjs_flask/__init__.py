51141f39d5b725e9ba630af74c820dceb5c60f68
pycodi <pycodi@hotmail.com>
2017-09-11 16:47:47 +0300

    upd
    upd

--- sockjs_flask/__init__.py
+++ sockjs_flask/__init__.py
@@ -15,13 +15,11 @@ from .protocol import MSG_CLOSE
 from .protocol import MSG_CLOSED
 from .route import get_manager, add_endpoint
 
-__version__ = '0.1.0'
+__version__ = '0.1.9'
 __author__ = 'Kryuchkov Nikita <pycodi@hotmail.com>'
 __all__ = (
     'get_manager', 'add_endpoint', 'Session', 'SessionManager',
-    'SessionIsClosed', 'SessionIsAcquired',
-    'STATE_NEW', 'STATE_OPEN', 'STATE_CLOSING', 'STATE_CLOSED',
-    'MSG_OPEN', 'MSG_MESSAGE', 'MSG_CLOSE', 'MSG_CLOSED',)
+    'SessionIsClosed', 'SessionIsAcquired', 'STATE_NEW', 'STATE_OPEN', 'STATE_CLOSING', 'STATE_CLOSED', 'MSG_OPEN', 'MSG_MESSAGE', 'MSG_CLOSE', 'MSG_CLOSED',)
 
 
 
